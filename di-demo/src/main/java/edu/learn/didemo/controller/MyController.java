d9816c4afb803e10d3eb18d3e5c1a9b6ffa54577
tejas.sheth <tejas.sheth@eclinicalworks.com>
2019-09-23 09:37:23 -0400

    remove sysout with logger

--- di-demo/src/main/java/edu/learn/didemo/controller/MyController.java
+++ di-demo/src/main/java/edu/learn/didemo/controller/MyController.java
@@ -1,11 +1,18 @@
 package edu.learn.didemo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    Logger logger = LoggerFactory.getLogger(MyController.class);
     public String hello(){
-        System.out.println("Hello!!!!");
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
         return  "foo";
     }
 }
