8a04c9b5f7d2a0f35aee9e7cbc481f61325a3fbb
tejas.sheth <tejas.sheth@eclinicalworks.com>
2019-09-23 09:30:42 -0400

    Create new Controller and perform simple Dependency Injection using ApplicationContext

---
+++ di-demo/src/main/java/edu/learn/didemo/controller/MyController.java
@@ -0,0 +1,11 @@
+package edu.learn.didemo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String hello(){
+        System.out.println("Hello!!!!");
+        return  "foo";
+    }
+}
